2a738f1c3c9a19364e81e251c957aac821c3c70e
EDE.POOJITH GOWD <151959723+poojithgowd@users.noreply.github.com>
2024-05-27 22:55:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Weather from './Weather'
+import axios from 'axios'
+import './Styles.css'
+const App = () => {
+  const[weather,setweather]=useState()
+  const [city,setCity]=useState('')
+  const[error,setError] = useState(null)
+  const apikey='d5ea5784119f527f9258faad5d37bf36';
+  const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}`;
+
+
+  const submit=(e)=>{
+    e.preventDefault()//
+     axios.get(url)
+    .then(response => {
+      setweather(response.data);})
+
+      
+    .catch(error => {
+        setError(error);
+        setweather(null); });
+    }
+  return (
+    <div className='bg'>
+    <div className='card'>
+      <h1 >WEATHER</h1>
+      <form >
+     
+        <input type='text' id='city' onChange={(e)=>{setCity(e.target.value)}}  placeholder='Enter city name'/> 
+        <button onClick={submit}>Get</button>
+      
+      <Weather weather={weather} error={error}/>
+      </form>
+      
+     
+    </div>
+    </div>
+  )
+}
+
+export default App
